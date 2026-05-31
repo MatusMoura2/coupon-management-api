d8c960260f65ef91a21c4ee3d6997b1dda1dde9d
MatusMoura2 <matus.moura333x2@gmail.com>
2026-05-31 15:10:03 -0300

    Primeiro commit

---
+++ src/main/java/com/couponmanagementapi/CouponManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.couponmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couponmanagementapi/CouponManagementApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.couponmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CouponManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
